69c0c8f8e8db76da50e4c93572a834a21d93a61a
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-02-14 22:14:51 +0100

    regression

--- implementations/linear_regression.py
+++ implementations/linear_regression.py
@@ -1,34 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 
 def loss(points, a, b):
     loss = 0.0
-    n = len(points)
+    n = len(points[0])
 
     for i in range(n):
         x = points[0][i]
         y = points[1][i]
         loss += (y - (a * x + b)) ** 2
 
-    return loss / float(len(points))
+    return loss / float(n)
 
 
 def fit(points, a, b, L):
-    n = len(points)
+    n = len(points[0])
 
-    a_gradient = 0
-    b_gradient = 0
+    a_delta = 0.0
+    b_delta = 0.0
 
     for i in range(n):
         x = points[0][i]
         y = points[1][i]
 
-        a_gradient += -(2 / n) * x * (y - (a * x + b))
-        b_gradient += -(2 / n) * (y - (a * x + b))
+        a_delta += (2 / n) * (y - (a * x + b)) * (-x)
+        b_delta += (2 / n) * (y - (a * x + b)) * (-1)
 
-    new_a = a - a_gradient * L
-    new_b = b - b_gradient * L
+    new_a = a - a_delta * L
+    new_b = b - b_delta * L
 
     return new_a, new_b
 
@@ -36,27 +37,41 @@ def fit(points, a, b, L):
 # main
 a = 0.0
 b = 0.0
-L = 0.00001
+L = 0.00000001
 
 points = np.array([
-    [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
-    [ 1, 2.4, 2.7, 3.4, 5.5, 6.6, 6.8, 8.1, 8, 11 ]
+    [i for i in range(10)],
+    [np.random.randint(0, 10) for i in range(10)]
 ])
 
-for _ in range(100000):
+for i in range(1000000):
     a, b = fit(points, a, b, L)
 
-print(loss(points, a, b))
-print(a, b)
+# i = 0
+# while True:
+#     i += 1
+#     l1 = loss(points, a, b)
+#     a, b = fit(points, a, b, L)
+#     l2 = loss(points, a, b)
+#     # print(l1)
+#     if (l1 < l2):
+#         break
 
-x = np.linspace(points.min() - 1, points.max() + 1, 100)
+# print(f"Iterations {i}")
+print(f"a: {a}\nb: {b}")
+print(f"Loss: {loss(points, a, b)}")
+
+model = LinearRegression()
+model.fit(points[0].reshape(-1, 1), points[1].reshape(-1, 1))
+
+x = np.linspace(points.min() - 1, points.max() + 1, 30)
 y = a * x + b
 
 # plot
 plt.title("Regressione Lineare")
+plt.ylim(-10, 40)
 
 plt.scatter(points[0], points[1], color="black")
 plt.plot(x, y, color="red")
-
+plt.plot(x, model.predict(x.reshape(-1, 1)), color="green")
 plt.show()
-
