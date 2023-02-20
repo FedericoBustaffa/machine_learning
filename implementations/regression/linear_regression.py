0839bf1e43744e8a7980c0034fee626706b57612
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-02-20 03:25:33 +0100

    polynomial regression

--- implementations/regression/linear_regression.py
+++ implementations/regression/linear_regression.py
@@ -3,60 +3,58 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def error(points, a, b, c):
+def error(points, a, b):
     total_error = 0.0
     n = len(points[0])
 
     for i in range(n):
         x = points[0][i]
         y = points[1][i]
-        total_error += (y - (a * x * x + b * x + c)) ** 2
+        total_error += (y - (a * x + b)) ** 2
 
     return total_error / float(n)
 
 
-def fit(points, a_now, b_now, c_now, L):
+def fit(points, a_now, b_now, L):
     n = len(points[0])
 
     a_gradient = 0
     b_gradient = 0
-    c_gradient = 0
 
     for i in range(n):
         x = points[0][i]
         y = points[1][i]
 
-        a_gradient += (y - (a_now * x * x + b_now * x + c)) * x * x
-        b_gradient += (y - (a_now * x * x + b_now * x + c)) * x
-        c_gradient += (y - (a_now * x * x + b_now * x + c))
+        a_gradient += (y - (a_now * x + b_now)) * x
+        b_gradient += (y - (a_now * x + b_now))
 
     new_a = a_now + a_gradient * L
     new_b = b_now + b_gradient * L
-    new_c = c_now + c_gradient * L
 
-    return new_a, new_b, new_c
+    return new_a, new_b
 
 
 def predict(a, b, x):
-    return a * x * x + b * x + c
+    return a * x + b
 
 
 if __name__ == "__main__":
+    # parametri iniziali
     a = 0
     b = 0
-    c = 0
-    L = 0.05
+    L = 0.005
     epochs = 5000
 
     # dataset
-    data = pd.read_csv("dataset1.csv")
+    data = pd.read_csv("../datasets/dataset1.csv")
     x_values = data.x.values
     y_values = data.y.values
     points = np.array([x_values, y_values])
 
+    print(error(points, a, b))
     # fitting
     for i in range(epochs):
-        a, b, c = fit(points, a, b, c, L)
+        a, b = fit(points, a, b, L)
 
         if i % 75 == 0:
             x = np.linspace(0, 1.1, 50)
@@ -64,7 +62,7 @@ if __name__ == "__main__":
 
             # plot
             plt.clf()
-            plt.title("Regressione Polinomiale")
+            plt.title("Regressione Lineare")
             plt.xlim(0, 1.1)
             plt.ylim(0, 1)
             plt.scatter(points[0], points[1], color="black")
@@ -74,7 +72,6 @@ if __name__ == "__main__":
     # risultato
     print(f"A: {a}")
     print(f"B: {b}")
-    print(f"C: {c}")
-    print(f"Errore: {error(points, a, b, c)}")
+    print(f"Errore: {error(points, a, b)}")
 
     plt.show()
---
+++ implementations/regression/polynomial_regression.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+
+def predict(coef, x):
+    y = 0
+    e = len(coef) - 1
+    for c in coef:
+        y += c * (x ** e)
+        e -= 1
+
+    return y
+
+
+def error(points, coef):
+    total_error = 0.0
+    n = len(points[0])
+
+    for i in range(n):
+        x = points[0][i]
+        y = points[1][i]
+        total_error += (y - (predict(coef, x))) ** 2
+
+    return total_error / float(n)
+
+
+def fit(points, coef, L):
+    n = len(points[0])
+
+    gradients = [0] * len(coef)
+    for i in range(n):
+        x = points[0][i]
+        y = points[1][i]
+
+        e = len(coef) - 1
+        for i in range(len(gradients)):
+            gradients[i] += (y - predict(coef, x)) * (x**e)
+            e -= 1
+
+    for i in range(len(coef)):
+        coef[i] = coef[i] + gradients[i] * L
+
+    return coef
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 5:
+        print(f"USAGE: py {sys.argv[0]} <degree> <L> <epochs> <dataset>")
+        quit()
+
+    # configurazione
+    degree = int(sys.argv[1])
+    L = float(sys.argv[2])
+    epochs = int(sys.argv[3])
+    dataset = sys.argv[4]
+
+    coef = [0 for _ in range(degree + 1)]
+
+    # dataset
+    data = pd.read_csv(f"../datasets/{dataset}")
+    x_values = data.x.values
+    y_values = data.y.values
+    points = np.array([x_values, y_values])
+
+    # fitting
+    mean = points.mean()
+    x_min = points[0].min()
+    x_max = points[0].max()
+    y_min = points[1].min()
+    y_max = points[1].max()
+
+    for i in range(epochs):
+        coef = fit(points, coef, L)
+
+        if i % 75 == 0:
+            x = np.linspace(x_min - mean, x_max + mean, 100)
+            y = predict(coef, x)
+
+            # plot
+            plt.clf()
+
+            plt.title("Regressione Polinomiale")
+            plt.xlim(x_min - mean, x_max + mean)
+            plt.ylim(y_min - mean, y_max + mean)
+            plt.grid()
+
+            plt.scatter(points[0], points[1], color="black")
+            plt.plot(x, y, color="red")
+
+            plt.pause(0.0001)
+
+    # risultato
+    print(f"Errore: {error(points, coef)}")
+
+    plt.show()
