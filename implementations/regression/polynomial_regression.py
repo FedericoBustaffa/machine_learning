986ddb8dc8dd00f6f63e115979ef2f53a9553f28
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-02-24 01:25:55 +0100

    regression

--- implementations/regression/polynomial_regression.py
+++ implementations/regression/polynomial_regression.py
@@ -9,42 +9,21 @@ def predict(coef, x):
     powers = np.arange(len(coef))
     for i in range(len(x)):
         y[i] = np.sum(coef * x[i]**powers)
-    #y = 0
-    #e = len(coef) - 1
-    #for c in coef:
-    #    y += c * (x ** e)
-    #    e -= 1
+
     return y
 
 
 def error(points, coef):
-    #total_error = 0.0
-    #n = len(points[0])
-
-    #for i in range(n):
-    #    x = points[0][i]
-    #    y = points[1][i]
-    #    total_error += (y - (predict(coef, x))) ** 2
-
     total_error = (points[1] - predict(coef, points[0]))**2
     return np.mean(total_error)
 
 
 def fit(points, coef, L):
-    #n = len(points[0])
-
     gradients = np.zeros(len(coef))
+    x = points[0]
+    y = points[1]
     for i in range(len(coef)):
-        gradients[i] = - 2 * np.mean( (points[1] - predict(coef, points[0])) * points[0]**i )
-    #for i in range(n):
-    #    x = points[0][i]
-    #    y = points[1][i]
-
-    #    e = len(coef) - 1
-    #    for i in range(len(gradients)):
-    #        gradients[i] += (y - predict(coef, x)) * (x**e)
-    #        e -= 1
-
+        gradients[i] = - 2 * np.mean((y - predict(coef, x)) * x**i)
     coef = coef - gradients * L
 
     return coef
@@ -62,15 +41,12 @@ if __name__ == "__main__":
     epochs = int(sys.argv[3])
     dataset = sys.argv[4]
 
-    #coef = [0 for _ in range(degree + 1)]
     coef = np.zeros(degree + 1)
 
     # dataset
-    data = pd.read_csv(dataset)
-    #x_values = data.x.values
-    #y_values = data.y.values
-    #points = np.array([x_values, y_values])
+    data = pd.read_csv(f"../datasets/{dataset}")
     points = data.to_numpy().T
+    print(points)
 
     # samples per il plot
     x_min = points[0].min()
@@ -79,16 +55,16 @@ if __name__ == "__main__":
     # effective learning rate (learning rate for each sample)
     L_eff = L / len(points[0])
 
-    plt.plot(np.linspace(x_min, x_max, 100), predict(coef,np.linspace(x_min, x_max, 100)))
-    plt.grid()
+    x_samples = np.linspace(x_min, x_max, 100)
+    y_samples = predict(coef, x_samples)
 
     # fitting
     for i in range(epochs):
         coef = fit(points, coef, L_eff)
 
         if i % 50 == 0:
-            x = np.linspace(x_min, x_max, 100)
-            y = predict(coef, x)
+            x_samples = np.linspace(x_min, x_max, 100)
+            y_samples = predict(coef, x_samples)
 
             # plot
             plt.clf()
@@ -97,9 +73,9 @@ if __name__ == "__main__":
             plt.grid()
 
             plt.scatter(points[0], points[1], color="black")
-            plt.plot(x, y, color="red")
+            plt.plot(x_samples, y_samples, color="red")
 
-            plt.pause(0.1)
+            plt.pause(0.00001)
 
     # risultato
     print(f"Errore: {error(points, coef)}")
